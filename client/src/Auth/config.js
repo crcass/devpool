47e6d28f5083be52571701d41656bdf23d302c65
crcass <ccass@live.com>
2019-07-08 19:48:12 -0500

    update firebase config - temporary

--- client/src/Auth/config.js
+++ client/src/Auth/config.js
@@ -1,11 +1,11 @@
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: 'AIzaSyBIGNMppLIsLKzxDWhA8Sr9itX3zlACLVU',
+  authDomain: 'devpool2-9e6e8.firebaseapp.com',
+  databaseURL: 'https://devpool2-9e6e8.firebaseio.com',
+  projectId: 'devpool2-9e6e8',
+  storageBucket: '',
+  messagingSenderId: '252058802428',
+  appId: '1:252058802428:web:f0a0adb0e3af45c8'
 };
 
 export default config;
